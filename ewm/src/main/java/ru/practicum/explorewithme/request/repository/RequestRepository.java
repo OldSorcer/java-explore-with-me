0d87e79479cff2c5dc086bf473ceffb9b193c88c
Роман Рахипов <roman.zer@yandex.ru>
2022-12-01 09:55:56 +0300

    style: удалены неиспользуемые импорты

--- ewm/src/main/java/ru/practicum/explorewithme/request/repository/RequestRepository.java
+++ ewm/src/main/java/ru/practicum/explorewithme/request/repository/RequestRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.explorewithme.request.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.explorewithme.request.model.Participation;
-import ru.practicum.explorewithme.request.model.ParticipationStatus;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +12,7 @@ import java.util.Optional;
 public interface RequestRepository extends JpaRepository<Participation, Long> {
     /**
      * Метод поиска всех запросов на участие в событии по числовому идентификатору события.
+     *
      * @param eventId числовой идентификатор события.
      * @return список заявок на участие в событии.
      */
@@ -21,6 +21,7 @@ public interface RequestRepository extends JpaRepository<Participation, Long> {
     /**
      * Метод поиска всех заявок на участие в событии по числовому идентификатору
      * пользователя оставившего заявку.
+     *
      * @param requesterId числовой идентификатор пользователя оставившего заявку на участие в событии.
      * @return список заявок на участие в событии.
      */
@@ -29,8 +30,9 @@ public interface RequestRepository extends JpaRepository<Participation, Long> {
     /**
      * Метод поиска заявки на участие в событии по числовому идентификатору пользователя создавшего заявку и
      * числовому идентификатору события.
+     *
      * @param requesterId числовой идентификатор пользователя создавшего заявку на участие в событии;
-     * @param eventId числовой идентификатор события.
+     * @param eventId     числовой идентификатор события.
      * @return объект заявки на участие в событии.
      */
     Optional<Participation> findByRequesterIdAndEventId(long requesterId, long eventId);
