b8cf19e4121fcee6fb7c895c1338f86bc1ddb8d0
Роман Рахипов <roman.zer@yandex.ru>
2022-12-03 17:03:30 +0300

    fix: удален эндпоинт редактирования комментария из AdminCommentController

--- ewm/src/main/java/ru/practicum/explorewithme/comment/controller/AdminController.java
+++ ewm/src/main/java/ru/practicum/explorewithme/comment/controller/AdminController.java
@@ -4,16 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.practicum.explorewithme.comment.dto.CommentDto;
-import ru.practicum.explorewithme.comment.dto.CommentTextDto;
 import ru.practicum.explorewithme.comment.service.AdminCommentService;
 
-import javax.validation.Valid;
 import javax.validation.constraints.Min;
 
 @RestController
@@ -23,15 +18,6 @@ import javax.validation.constraints.Min;
 @Slf4j
 public class AdminController {
     private final AdminCommentService commentService;
-
-    @PatchMapping("/{commentId}")
-    public CommentDto update(@Valid @RequestBody CommentTextDto commentTextDto,
-                             @PathVariable @Min(1) long commentId) {
-        log.info("[ewm-service] Получен GET запрос к эндпоинту /admin/comments/{} \n" +
-                "Тело запроса: {}",
-                commentId, commentTextDto);
-        return commentService.update(commentTextDto, commentId);
-    }
 
     @DeleteMapping("/{commentId}")
     public void delete(@PathVariable @Min(1) long commentId) {
